27a70ad1cdba9ce7fb1c77a0569b0ab4e9dc9302
Felipe <115326966+felipemanganelli@users.noreply.github.com>
2024-03-13 22:24:39 -0300

    A3 - ExcecaoUsuario

---
+++ Main.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			throw new MinhaExcecao("Esta é uma exceção definida pelo usuário!");
+		} catch (MinhaExcecao e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
---
+++ MinhaExcecao.java
@@ -0,0 +1,7 @@
+package main;
+
+public class MinhaExcecao extends Exception {
+	public MinhaExcecao(String mensagem) {
+		super(mensagem);
+	}
+}
