3cbad581a3ff2e07805e290250fc90839ac1f97c
pamela dantas <112967736+pameladantasp@users.noreply.github.com>
2024-03-27 15:57:32 -0300

    Delete Nivel Basico directory

--- Nivel Basico/Livro/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Livro livro = new Livro("Senhor dos Anéis", "J.R.R. Tolkien", 1954);
-        livro.imprimirDetalhes();
-    }
-}
